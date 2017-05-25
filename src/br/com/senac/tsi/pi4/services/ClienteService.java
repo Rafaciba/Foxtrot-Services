2fb20555a9d5897a18bbd70ddbc474fca78db769
kymera28 <tobiasponce28@gmail.com>
2017-05-24 22:08:15 -0300

    Cliente Services atualizado

--- src/br/com/senac/tsi/pi4/services/ClienteService.java
+++ src/br/com/senac/tsi/pi4/services/ClienteService.java
@@ -2,6 +2,7 @@ package br.com.senac.tsi.pi4.services;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -21,24 +22,34 @@ public class ClienteService {
 		try {
 			
 			Connection conn = Database.get().conn();
-			PreparedStatement ps = conn.prepareStatement("INSERT INTO Cliente (nomeCompletoCliente, emailCliente, senhaCliente, CPFCliente,"
-					+ " celularCliente, telComercialCliente, telResidencialCliente, dtNascCliente, recebeNewsLetter) "
-					+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-			ps.setString(1, cliente.getNomeCompletoCliente());
-			ps.setString(2, cliente.getEmailCliente());
-			ps.setString(3, cliente.getSenhaCliente());
-			ps.setString(4, cliente.getCpfCliente());
-			ps.setString(5, ((cliente.getCelularCliente()!=null)?cliente.getCelularCliente():""));
-			ps.setString(6, ((cliente.getTelComercialCliente()!=null)?cliente.getTelComercialCliente():""));
-			ps.setString(7, ((cliente.getTelResidencialCliente()!=null)?cliente.getTelResidencialCliente():""));
-			ps.setString(8, ((cliente.getDtNascCliente()!=null)?cliente.getDtNascCliente():""));
-			ps.setInt(9, cliente.getRecebeNewsLetter());
-			int rs = ps.executeUpdate();
-			if(rs == 0){
-				return Response.status(500).entity("Erro na query").build();
+			PreparedStatement preparedStatement = conn.prepareStatement("SELECT emailCliente FROM Cliente WHERE emailCliente = ?");
+			preparedStatement.setString(1, cliente.getEmailCliente());
+			
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			if(!resultSet.next()){
+				PreparedStatement ps = conn.prepareStatement("INSERT INTO Cliente (nomeCompletoCliente, emailCliente, senhaCliente, CPFCliente,"
+						+ " celularCliente, telComercialCliente, telResidencialCliente, dtNascCliente, recebeNewsLetter) "
+						+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+				ps.setString(1, cliente.getNomeCompletoCliente());
+				ps.setString(2, cliente.getEmailCliente());
+				ps.setString(3, cliente.getSenhaCliente());
+				ps.setString(4, cliente.getCpfCliente());
+				ps.setString(5, ((cliente.getCelularCliente()!=null)?cliente.getCelularCliente():""));
+				ps.setString(6, ((cliente.getTelComercialCliente()!=null)?cliente.getTelComercialCliente():""));
+				ps.setString(7, ((cliente.getTelResidencialCliente()!=null)?cliente.getTelResidencialCliente():""));
+				ps.setString(8, ((cliente.getDtNascCliente()!=null)?cliente.getDtNascCliente():""));
+				ps.setInt(9, cliente.getRecebeNewsLetter());
+				int rs = ps.executeUpdate();
+				if(rs == 0){
+					return Response.status(500).entity("Erro ao executar a requisição").build();
+				}else{
+					return Response.status(201).entity("Cadastrado com sucesso!").build();
+				}
 			}else{
-				return Response.status(200).entity("DEU CERTO").build();
+				return Response.status(200).entity("Email já utilizado").build();
 			}
+				
 			
 		} catch (Exception e) {
 			e.printStackTrace();
